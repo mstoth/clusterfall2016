ec15b10cb027eb985eb0f34a7a938dec7962a337
Michael Toth <michael@virtualpianist.com>
2016-06-25 14:09:22 -0400

    green 3

--- Maze.py
+++ Maze.py
@@ -1,4 +1,7 @@
 import turtle
 class Maze():
-    def __init__(self):
+    def __init__(self,size=420):
         self.s = turtle.Screen()
+        self.size=size
+        self.s.window_width = self.size
+        self.s.window_height = self.size
