4ad866df39a3b2ebb6ac1b9e29c236985066c854
Michael Toth <michael@virtualpianist.com>
2016-06-26 10:46:20 -0400

    green 8

--- Maze.py
+++ Maze.py
@@ -14,3 +14,11 @@ class Maze():
         self.matrix = [[1 for i in range(21)] for j in range(21)]
         self.t.goto(-(self.size/2-10),self.size/2-10)
         self.matrix[0][0]=0
+
+    def getMatrixValueAt(self,pos):
+        x=int(pos[0]+self.size/2)/20
+        y=(self.size/20)-int((pos[1]+self.size/2)/20)-1
+        if x < 0 or y < 0 or x > self.size/20-1 or y > self.size/20-1:
+            return -1
+        v=self.matrix[x][y]
+        return v
