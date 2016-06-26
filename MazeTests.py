1c49f131d4a1ae9e8dd5be8ce617baba5cf14144
Michael Toth <michael@virtualpianist.com>
2016-06-26 10:26:53 -0400

    Red 7

--- MazeTests.py
+++ MazeTests.py
@@ -2,24 +2,29 @@ from Maze import *
 import turtle
 import unittest
 
+SIZE = 420
+
 class testMaze(unittest.TestCase):
     
     def setUp(self):
         # this checks for a Maze class
-        self.m=Maze()
+        self.m=Maze(SIZE)
 
-         
     def testScreenExists(self):
         assert type(self.m.s) == turtle._Screen
-        assert self.m.s.window_width == 420
+        assert self.m.s.window_width == self.m.size
         assert self.m.s.bgcolor() == 'blue'
 
     def testTurtleExists(self):
         assert type(self.m.t) == turtle.Turtle
 
     def testForMatrix(self):
-        assert len(self.m.matrix)==21
+        assert len(self.m.matrix)==SIZE/20
 
+    def testReset(self):
+        self.m.reset()
+        assert self.m.matrix[0][0]==0
+        assert self.m.t.pos()==(-(SIZE/2-10),SIZE/2-10)
 
 
 if __name__=='__main__':
