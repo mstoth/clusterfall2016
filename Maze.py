71e2937968593f526fe9a1d037bd12b97cf17002
Michael Toth <michael@virtualpianist.com>
2016-06-26 10:34:15 -0400

    green 7

--- Maze.py
+++ Maze.py
@@ -1,12 +1,16 @@
 import turtle
 class Maze():
     def __init__(self,size=420):
-        self.s = turtle.Screen()
         self.size=size
+        self.reset()
+
+    def reset(self):
+        self.s = turtle.Screen()
         self.s.window_width = self.size
         self.s.window_height = self.size
         self.s.bgcolor('blue')
         self.t = turtle.Turtle()
         self.t.penup()
         self.matrix = [[1 for i in range(21)] for j in range(21)]
-
+        self.t.goto(-(self.size/2-10),self.size/2-10)
+        self.matrix[0][0]=0
