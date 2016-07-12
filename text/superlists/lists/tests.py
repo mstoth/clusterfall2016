d47bfdf26d1c8483b7979cfc1f9323cf61eb38db
Michael Toth <michael@virtualpianist.com>
2016-07-12 18:36:18 -0400

    chapter 3

--- text/superlists/lists/tests.py
+++ text/superlists/lists/tests.py
@@ -3,10 +3,20 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from lists.views import home_page 
+from django.http import HttpRequest
+
 
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
-        print(type(found))
-        self.assertEqual(found.func, home_page)  
+        self.assertEqual(found.func, home_page)
+
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  #1
+        print(type(request))
+        response = home_page(request)  #2
+        self.assertTrue(response.content.startswith(b'<html>'))  #3
+        self.assertIn(b'<title>To-Do lists</title>', response.content)  #4
+        self.assertTrue(response.content.endswith(b'</html>'))  #5
