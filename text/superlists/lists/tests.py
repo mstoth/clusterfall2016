910124696ecaf47e35a6e20fceeb365e308404a8
Michael Toth <michael@virtualpianist.com>
2016-07-14 19:42:15 -0400

    chapter 5

--- text/superlists/lists/tests.py
+++ text/superlists/lists/tests.py
@@ -18,4 +18,17 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
+        self.assertTrue(response.content.strip().endswith(b'</html>'))
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text':  'A new list item'}
+        )
         self.assertEqual(response.content.decode(), expected_html)
+
