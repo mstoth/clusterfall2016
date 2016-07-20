03c76e0e7dd5bfd8359b036c11ce251500457d21
Michael Toth <michael@virtualpianist.com>
2016-07-20 14:00:50 -0400

    add item

--- text/superlists/superlists/urls.py
+++ text/superlists/superlists/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from lists import views
 
 urlpatterns = [
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
-    url(r'^lists/new$', views.new_list, name='new_list'),
+    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+    url(r'^lists/new$', views.new_list, name='new`_list'),
     url(r'^$', views.home_page, name='home'),
     # url(r'^admin/', admin.site.urls),
 ]
